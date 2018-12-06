d1529ceb0da80e6516940c8c57937debad644092
Matthias Eiletz <maei@Matthiass-MBP.fritz.box>
2018-12-06 20:56:10 +0100

    docker flask mongodb restful_api

--- web/app.py
+++ web/app.py
@@ -1,45 +1,33 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
+# Imports für pymongo
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://db:27017") # Wie im docker-compose.yml
+
+# Datenbank erstellen
+db = client.aNewDB
+UserNum = db["UserNum"]
+
+# Document
+UserNum.insert({
+    'num_of_users': 0
+})
+
 app = Flask(__name__)
 
 api = Api(app)
 
-_parser = reqparse.RequestParser()
-_parser.add_argument('x', type=float, required=True, help='X must be provided and float')
-_parser.add_argument('y', type=float, required=True, help='Y must be provided and float')
+class Visit(Resource):
+    def get(self):
+        prev_num = UserNum.find({})[0]['num_of_users']
+        new_num = prev_num + 1
+        UserNum.update({}, {"$set":{"num_of_users": new_num}})
 
-class Plus(Resource):
-    def post(self):
-        data = _parser.parse_args()
-        result = data['x'] + data['y']
-        return {'result': result}, 200
+        return str("Hello User"+ str(new_num))
 
-class Sub(Resource):
-    def post(self):
-        data = _parser.parse_args()
-        result = data['x'] - data['y']
-        return {'result': result}, 200
-
-class Div(Resource):
-    def post(self):
-        data = _parser.parse_args()
-        if data['y'] == 0:
-            return {'Error': 'y kleinergleich 0'}, 301
-        result = data['x'] / data['y']
-        return {'result': result}, 200
-
-class Mul(Resource):
-    def post(self):
-        data = _parser.parse_args()
-        result = data['x'] * data['y']
-        return {'result': result}, 200
-
-
-api.add_resource(Plus, '/plus')
-api.add_resource(Sub, '/sub')
-api.add_resource(Div, '/div')
-api.add_resource(Mul, '/mul')
+api.add_resource(Visit, '/hello')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
